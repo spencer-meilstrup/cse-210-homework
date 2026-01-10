4e20c319e5a8ba1221a010b0c02bd3b5ca1b9c03
Spencer Meilstrup <mei20002@byui.edu>
2026-01-09 19:46:09 -0700

    did the James Bond activity for Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,12 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.Write($"Your name is {last}, {first} {last}.");
     }
 }
