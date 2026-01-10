c98fc3fd2c11bd0e1a4c8eaea3669f77185c1b86
Spencer Meilstrup <mei20002@byui.edu>
2026-01-09 20:33:25 -0700

    did the grade percentage assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "F";
+        Console.Write("What grade percentage did you get? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        string gradeSign = "";
+        int lastdigit = percentage % 10;
+        if (lastdigit >=7 && percentage <= 90 && percentage >= 60)
+        {
+            gradeSign = "+";
+        }
+        else if (lastdigit < 3 && percentage >= 60)
+        {
+            gradeSign = "-";
+        }
+        Console.WriteLine($"Your grade is {letter}{gradeSign}.");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Too bad you didn't pass. Good luck next time!");
+        }
     }
 }
