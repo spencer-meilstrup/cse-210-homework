5f5c351a219f6e4164d6ddb9165d1b6ac1ab7be7
Spencer Meilstrup <mei20002@byui.edu>
2026-01-13 17:19:57 -0700

    finished the Prep 3 activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response = "yes";
+        while (response == "yes")
+        {
+            string matchingNumber = "wrong";
+            Random randomGenerator = new Random();
+            int theNumber = randomGenerator.Next(1,100);
+            int turnCount = 0;
+            while (matchingNumber == "wrong")
+            {
+                Console.Write("What number am I thinking of? ");
+                string userInput = Console.ReadLine();
+                turnCount += 1;
+                int inputNumber = int.Parse(userInput);
+                if (inputNumber == theNumber)
+                {
+                    matchingNumber = "correct";
+                    Console.WriteLine($"You guessed my number in {turnCount} guesses!");
+                    Console.Write("Do you want to keep playing? ");
+                    response = Console.ReadLine();
+                }
+                else if (inputNumber < theNumber)
+                {
+                    Console.WriteLine("That guess is lower than my number.");
+                }
+                else
+                {
+                    Console.WriteLine("That guess is higher than my number.");
+                }
+            }
+        }
     }
 }
